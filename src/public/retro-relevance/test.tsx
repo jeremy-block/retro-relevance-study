fbe2b798fa271469b4e557783c1ba3a6a1796879
jeremy-block <jeremy.blockbuster@gmail.com>
2025-03-25 21:15:54 -0400

    Testing markdown text rendering as html elements.

--- src/public/retro-relevance/test.tsx
+++ src/public/retro-relevance/test.tsx
@@ -1,11 +1,16 @@
 import { useCallback } from "react";
 import { useStoreSelector } from "../../store/store";
 import { StimulusParams } from "../../store/types";
+import { markdownToHtml } from "./utils/markdownUtils";
 
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function test({ parameters, setAnswer }: StimulusParams<any>) {
-  const answers = useStoreSelector((state) => state.answers["AdminStart_0"].answer.mainSummary);
+  const answers = parameters.testingStimulusValue
+  // const answers = useStoreSelector((state) => state.answers["AdminStart_0"].answer.mainSummary);
+  console.log("🚀 ~ test ~ answers:", answers)
+
+  const markdwonTestText = markdownToHtml(String(answers))
 
     const { taskid } = parameters;
 
@@ -21,13 +26,14 @@ function test({ parameters, setAnswer }: StimulusParams<any>) {
           },
         });
       }, [setAnswer]);
-  console.log("🚀 ~ test ~ answers:", answers)
   return (
     <div>
-      <p>
-        {answers}
-      </p>
-      <button onClick={clickCallback}>
+      <div 
+        // ref={contentRef}
+        className="markdown-content"
+        dangerouslySetInnerHTML={{ __html: markdwonTestText }}
+      />
+            <button onClick={clickCallback}>
         <p style={{ height: 400 }}>
       Hello 
     </p>
